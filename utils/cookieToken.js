70b2479322b6e62e4dcf5ab67ac6176568cf1be3
Atmik Goswami <atmikcodes@gmail.com>
2024-05-30 13:43:22 +0530

    updated cookie token logic

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -3,7 +3,7 @@ const cookieToken = (user, res) => {
 
   const options = {
     expiresIn: new Date(
-      Date.now() + process.env.COOKIE_TOKEN * 24 * 3600 * 1000
+      Date.now() + process.env.COOKIE_TIME * 24 * 3600 * 1000
     ),
     httpOnly: true,
   };
